5906c18abc07c92dea4c466be01b3e70d0e36ca0
Mark Shead <mark.shead@xeric.net>
2020-03-24 10:46:44 -0500

    adding files

---
+++ src/test/java/net/xeric/demos/ProxyRequestHeaderIT.java
@@ -0,0 +1,30 @@
+package net.xeric.demos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Random;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProxyRequestHeaderIT {
+    @Autowired
+    WebDriver webdriver;
+    @Test
+    public void testHeaders() throws Exception {
+        webdriver.get("https://manytools.org/http-html-text/http-request-headers/");
+        assertTrue(webdriver.getPageSource().contains("Chrome"));
+        assertTrue(webdriver.getPageSource().contains("My-Header"));
+        assertTrue(webdriver.getPageSource().contains("My-Header-Value"));
+
+
+    }
+
+}
---
+++ src/test/java/net/xeric/demos/TestConfig.java
@@ -0,0 +1,57 @@
+package net.xeric.demos;
+
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.filters.RequestFilter;
+import net.lightbody.bmp.util.HttpMessageContents;
+import net.lightbody.bmp.util.HttpMessageInfo;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * This is the Spring configuration file that allows us to get the webdriver.
+ */
+@Configuration
+public class TestConfig {
+
+    private static final String MAC_CHROME_DRIVER = "./src/test/resources/bin/chromedriver";
+    private static final String WINDOWS_CHROME_DRIVER = "./src/test/resources/bin/chromedriver.exe";
+
+    @Bean(destroyMethod = "quit")
+    @Lazy
+    @Scope("singleton")
+    public WebDriver getWebDriver() {
+
+        BrowserMobProxy proxy = new BrowserMobProxyServer();
+        proxy.start(0);
+        int port = proxy.getPort();
+        proxy.addHeader("My-Header", "My-Header-Value");
+        Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+
+
+
+        // configure it as a desired capability
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
+
+
+        String driverPath = isWindows()? WINDOWS_CHROME_DRIVER : MAC_CHROME_DRIVER;
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        return new ChromeDriver(capabilities);
+    }
+
+    private boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("win");
+    }
+
+}
